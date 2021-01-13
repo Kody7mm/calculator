a09442f4a756a6c495e03a56b1eec06f4d40e00e
Kody Gordon <gordon.km89@gmail.com>
2021-01-13 13:11:13 -0800

    added addition information to HTLM and JS files

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,22 @@
-function add(number1, number2) {
-	return number1 + number2;
-}
+const add = function(number1, number2) {
+  return number1 + number2;
+};
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+
+  alert(add(number1, number2));
+});
